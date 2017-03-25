7d08632ae425d98afa1658aa27803a2f5deee1dd
mpulte <marcpulte@gmail.com>
2017-03-25 18:37:28 -0400

    Added profanity filter reply settings

--- src/main/java/com/discordbot/gui/ProfanityFilterController.java
+++ src/main/java/com/discordbot/gui/ProfanityFilterController.java
@@ -1,22 +1,23 @@
 package com.discordbot.gui;
 
 import com.discordbot.model.ProfanityFilter;
+import com.discordbot.util.ProfanityFilterListener;
+import com.discordbot.util.SettingsManager;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.CheckBox;
 import javafx.scene.control.TextField;
-import net.dv8tion.jda.core.utils.SimpleLog;
 import org.controlsfx.control.CheckComboBox;
 
 import java.net.URL;
+import java.security.InvalidKeyException;
 import java.util.Collection;
 import java.util.List;
 import java.util.ResourceBundle;
 import java.util.stream.Collectors;
 
 public class ProfanityFilterController implements FXMLController {
-
-    private static SimpleLog LOG = SimpleLog.getLog("ProfanityFilterController");
 
     @FXML
     public TextField addField;
@@ -26,6 +27,10 @@ public class ProfanityFilterController implements FXMLController {
     public CheckComboBox<String> removeComboBox;
     @FXML
     public Button removeButton;
+    @FXML
+    public CheckBox replyGuildCheckBox;
+    @FXML
+    public CheckBox replyPrivateCheckBox;
 
     private ProfanityFilter filter;
 
@@ -38,6 +43,20 @@ public class ProfanityFilterController implements FXMLController {
         addField.setOnAction(e -> handleAddAction());
         addButton.setOnAction(e -> handleAddAction());
         removeButton.setOnAction(e -> handleRemoveAction());
+        replyGuildCheckBox.setOnAction(e -> handleGuildCheckBox());
+        replyPrivateCheckBox.setOnAction(e -> handlePrivateCheckBox());
+
+        try {
+            replyGuildCheckBox.setSelected(SettingsManager.getBoolean(ProfanityFilterListener.SETTING_REPLY_GUILD));
+        } catch (InvalidKeyException e) {
+            SettingsManager.setBoolean(ProfanityFilterListener.SETTING_REPLY_GUILD, false);
+        }
+        try {
+            replyPrivateCheckBox.setSelected(SettingsManager.getBoolean(ProfanityFilterListener.SETTING_REPLY_PRIVATE));
+        } catch (InvalidKeyException e) {
+            SettingsManager.setBoolean(ProfanityFilterListener.SETTING_REPLY_PRIVATE, false);
+        }
+
         updateRemoveComboBox();
     }
 
@@ -83,6 +102,14 @@ public class ProfanityFilterController implements FXMLController {
         comboBoxList.addAll(toAdd);
         comboBoxList.removeAll(toRemove);
         comboBoxList.sort(String::compareTo);
+    }
+
+    private void handleGuildCheckBox() {
+        SettingsManager.setBoolean(ProfanityFilterListener.SETTING_REPLY_GUILD, replyGuildCheckBox.isSelected());
+    }
+
+    private void handlePrivateCheckBox() {
+        SettingsManager.setBoolean(ProfanityFilterListener.SETTING_REPLY_PRIVATE, replyPrivateCheckBox.isSelected());
     }
 
     @Override
--- src/main/java/com/discordbot/util/ProfanityFilterListener.java
+++ src/main/java/com/discordbot/util/ProfanityFilterListener.java
@@ -8,6 +8,7 @@ import net.dv8tion.jda.core.entities.User;
 import net.dv8tion.jda.core.events.message.MessageReceivedEvent;
 import net.dv8tion.jda.core.hooks.ListenerAdapter;
 
+import java.security.InvalidKeyException;
 import java.util.Collection;
 
 public class ProfanityFilterListener extends ListenerAdapter {
@@ -29,36 +30,50 @@ public class ProfanityFilterListener extends ListenerAdapter {
 
             Collection<String> words = filter.filter(message.getContent());
             if (!words.isEmpty()) {
-                System.out.println("Profanity Found");
+                // delete the message
                 message.delete().queue();
 
-                event.getChannel().sendMessage(
-                        new MessageBuilder()
-                                .append("Message from ")
-                                .append(author)
-                                .append(" deleted because it was naughty")
-                                .build())
-                        .queue();
+                // reply in guild if enabled
+                try {
+                    if (SettingsManager.getBoolean(SETTING_REPLY_GUILD)) {
+                        event.getChannel().sendMessage(
+                                new MessageBuilder()
+                                        .append("Message from ")
+                                        .append(author)
+                                        .append(" deleted because it was naughty")
+                                        .build())
+                                .queue();
+                    }
+                } catch (InvalidKeyException e) {
+                    SettingsManager.setBoolean(SETTING_REPLY_GUILD, false);
+                }
 
-                MessageBuilder messageBuilder = new MessageBuilder();
-                messageBuilder.append(words.size() > 1 ? "The words " : "The word ");
-                words.forEach(word -> messageBuilder.append(word).append(" "));
-                messageBuilder
-                        .append(words.size() > 1 ? "are " : "is ")
-                        .append("not allowed in channel ")
-                        .append(event.getChannel().getName())
-                        .append(" on ")
-                        .append(event.getGuild().getName());
+                // reply in private if enabled
+                try {
+                    if (SettingsManager.getBoolean(SETTING_REPLY_PRIVATE)) {
+                        MessageBuilder messageBuilder = new MessageBuilder();
+                        messageBuilder.append(words.size() > 1 ? "The words " : "The word ");
+                        words.forEach(word -> messageBuilder.append(word).append(" "));
+                        messageBuilder
+                                .append(words.size() > 1 ? "are " : "is ")
+                                .append("not allowed in channel ")
+                                .append(event.getChannel().getName())
+                                .append(" on ")
+                                .append(event.getGuild().getName());
 
-                // if we don't have a private channel open, we will have to open a new one
-                if (author.hasPrivateChannel()) {
-                    author.getPrivateChannel().sendMessage(messageBuilder.build()).queue();
-                } else {
-                    // best to do on new thread since we have to wait for private channel to be opened
-                    new Thread(() -> {
-                        author.openPrivateChannel().complete();
-                        author.getPrivateChannel().sendMessage(messageBuilder.build()).queue();
-                    }).run();
+                        // if we don't have a private channel open, we will have to open a new one
+                        if (author.hasPrivateChannel()) {
+                            author.getPrivateChannel().sendMessage(messageBuilder.build()).queue();
+                        } else {
+                            // best to do on new thread since we have to wait for private channel to be opened
+                            new Thread(() -> {
+                                author.openPrivateChannel().complete();
+                                author.getPrivateChannel().sendMessage(messageBuilder.build()).queue();
+                            }).run();
+                        }
+                    }
+                } catch (InvalidKeyException e) {
+                    SettingsManager.setBoolean(SETTING_REPLY_PRIVATE, false);
                 }
             }
         }
