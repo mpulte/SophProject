37104d71e02e3ec133728774058f4c96c0ccf97d
hrahmad <hrahmad@oakland.edu>
2017-03-29 12:34:59 -0400

    Added MemeCommand

--- src/main/java/com/discordbot/command/MemeCommand.java
+++ src/main/java/com/discordbot/command/MemeCommand.java
@@ -1,8 +1,24 @@
 package com.discordbot.command;
 
+import java.awt.Graphics;
+import java.awt.image.BufferedImage;
+import java.io.ByteArrayInputStream;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+
+import javax.imageio.ImageIO;
+
+import org.apache.commons.io.output.ByteArrayOutputStream;
+
+import net.dv8tion.jda.core.MessageBuilder;
 import net.dv8tion.jda.core.entities.ChannelType;
+import net.dv8tion.jda.core.entities.Message;
+import net.dv8tion.jda.core.entities.MessageChannel;
 
 public class MemeCommand extends CommandListener {
+	
+	private static final String[] extensions = {"png", "jpg", "jpeg", "gif"};
 
 	public MemeCommand(CommandHandler handler) {
 		super(handler);
@@ -10,26 +26,69 @@ public class MemeCommand extends CommandListener {
 
 	@Override
 	public void onCommandReceived(CommandReceivedEvent event) {
-		// TODO Auto-generated method stub
+		MessageChannel channel = event.getMessageReceivedEvent().getChannel();
+		java.util.List<String> args = event.getArgs();
+		
+		if (args.size() >= 2) {
+			String fileName = args.get(0);
+			
+			for (String ext : extensions) {
+				try {
+					File path = new File(System.getProperty("user.home") + "/meme/" + fileName + "." + ext);
+					final BufferedImage image = ImageIO.read(path);
 
+					Graphics g = image.getGraphics();
+					g.setFont(g.getFont().deriveFont(20f));
+					g.drawString(String.join(" " + "\n", args.subList(1, args.size())), 50, 450);
+					g.dispose();
+					
+					ByteArrayOutputStream baos = new ByteArrayOutputStream();
+					ImageIO.write(image, "png", baos);
+					InputStream is = new ByteArrayInputStream(baos.toByteArray());
+					
+					channel.sendFile(is, "Batman.png", (new MessageBuilder()).append("A message").build()).queue();
+					break;
+				} catch (IOException ignore) {
+					continue;
+				}
+				// TODO: meme doesn't exist
+			}
+		} else if (!args.isEmpty() && (args.get(0).equalsIgnoreCase("help") || args.get(0).equalsIgnoreCase("list"))) {
+			File folder = new File(System.getProperty("user.home") + "/meme");
+			File[] fileList = folder.listFiles();
+			
+			MessageBuilder messageBuilder = new MessageBuilder();
+			messageBuilder.append("Memes:");
+			
+			for (File file : fileList) {
+				String fileName = file.getName();
+				if (fileName.contains(".") && !fileName.endsWith(".")) {
+					String extension = fileName.substring(fileName.lastIndexOf('.') + 1);
+					for (String ext : extensions) {
+						if (ext.equals(extension)) {
+							messageBuilder.append("\n" + fileName.substring(0, fileName.lastIndexOf('.')));
+						}
+					}
+				}
+			}
+			
+			channel.sendMessage(messageBuilder.build()).queue();
+		}
 	}
 
 	@Override
 	public boolean usesChannel(ChannelType type) {
-		// TODO Auto-generated method stub
-		return false;
+		return true;
 	}
 
 	@Override
 	public String getDescription() {
-		// TODO Auto-generated method stub
-		return null;
+		return "Posts a meme with the caption you give it.";
 	}
 
 	@Override
 	public String getHelp() {
-		// TODO Auto-generated method stub
-		return null;
+		return "Type the name of the meme followed by the caption.";
 	}
 
 }
