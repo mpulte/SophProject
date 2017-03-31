bf698984744e7beeba2025189ba4b40c68afca77
mpulte <marcpulte@gmail.com>
2017-03-31 04:09:40 -0400

    Updated text image adder

--- src/main/java/com/discordbot/Main.java
+++ src/main/java/com/discordbot/Main.java
@@ -1,32 +1,91 @@
 package com.discordbot;
 
-import com.discordbot.command.CommandHandler;
-import com.discordbot.command.HelpCommand;
-import com.discordbot.command.KickCommand;
-import com.discordbot.command.MemeCommand;
-import com.discordbot.command.RollCommand;
-import com.discordbot.util.MessageListener;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.font.FontRenderContext;
+import java.awt.font.TextLayout;
+import java.awt.geom.AffineTransform;
+import java.awt.geom.Rectangle2D;
 
-import net.dv8tion.jda.core.JDA;
+public class Main extends JPanel {
 
-public class Main {
+    private static final String TOKEN = "MjY4OTQ3MzIyNTI2NzYwOTYx.C1iThg.TivcxnvyChBaMJ9wOZMXJsRQ1k0";
 
-	public static void main(String[] args) {
-		JDA jda = DiscordBot.getInstance().getJDA();
-		
-		if (jda == null) { // login failed, exit
-			System.exit(0);
-		}
-		
-		// TODO: create Class for building commandHandler
-		CommandHandler commandHandler = new CommandHandler();
-		commandHandler.addCommandListener("help", HelpCommand.class);
-		commandHandler.addCommandListener("roll", RollCommand.class);
-		commandHandler.addCommandListener("kick", KickCommand.class);
-		commandHandler.addCommandListener("meme", MemeCommand.class);
-		
-		jda.addEventListener(commandHandler);
-		jda.addEventListener(new MessageListener());
-	} // method main
+    Main() {
+        setPreferredSize(new Dimension(600, 600));
+    }
 
-} // class Main
+    public void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        String text = "A statement".toUpperCase();
+
+        g.setFont(new Font("Impact", Font.PLAIN, 20));
+        g.setColor(Color.BLUE);
+        g.fillRect(0, 0, getWidth(), getHeight());
+
+        Rectangle2D rectTop = new Rectangle2D.Double(10, 5, getWidth() - 10, getHeight() / 5);
+        Rectangle2D rectBot = new Rectangle2D.Double(10, getHeight() * 4 / 5 - 9, getWidth() - 10, getHeight() / 5);
+
+        addTextToGraphics((Graphics2D) g, rectTop, text, Position.TOP);
+        addTextToGraphics((Graphics2D) g, rectBot, text, Position.BOTTOM);
+
+        int x = getWidth() / 2;
+        int y = getHeight() / 2;
+        g.setColor(Color.RED);
+        g.drawLine(x, 0, x, getHeight());
+        g.drawLine(0, y, getWidth(), y);
+    }
+
+    public void addTextToGraphics(Graphics2D graphics2D, Rectangle2D bounds, String text, Position position) {
+        // create a new graphics so that we can add text without changing graphic2D's settings
+        Graphics2D graphics = (Graphics2D)graphics2D.create();
+        FontMetrics fontMetrics = graphics.getFontMetrics();
+
+//        Font font = graphics.getFont();
+        double scale = Math.min(bounds.getHeight() / fontMetrics.getHeight() * 0.95,
+                bounds.getWidth() / fontMetrics.stringWidth(text) * 0.95);
+        Font font = graphics.getFont().deriveFont(AffineTransform.getScaleInstance(scale, scale));
+        FontRenderContext frc = graphics.getFontRenderContext();
+        TextLayout textLayout = new TextLayout(text, font, frc);
+
+//        double scale = Math.min(bounds.getHeight() / (textLayout.getBounds().getHeight() - getAlignmentY()),
+//                bounds.getWidth() / (textLayout.getBounds().getWidth() - getAlignmentX()));
+
+        Shape outline = textLayout.getOutline(null);
+        Rectangle2D outlineBounds = outline.getBounds2D();
+
+//        AffineTransform transform = AffineTransform.getScaleInstance(scale, scale);
+        AffineTransform transform = new AffineTransform();
+        transform.translate(getWidth() / 2 - (outlineBounds.getWidth() / 2),
+                getHeight() / 2 + (outlineBounds.getHeight() / 2));
+        graphics.transform(transform);
+
+        // draw the shape
+        graphics.setColor(Color.WHITE);
+        graphics.fill(outline);
+        graphics.setColor(Color.BLACK);
+        graphics.draw(outline);
+    }
+
+    private enum Position { TOP, BOTTOM }
+
+    public static void main(String[] args) {
+        EventQueue.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+                } catch (ClassNotFoundException | InstantiationException | IllegalAccessException | UnsupportedLookAndFeelException ex) {
+                    ex.printStackTrace();
+                }
+
+                JFrame frame = new JFrame("test");
+                frame.add(new Main());
+                frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+                frame.pack();
+                frame.setLocationRelativeTo(null);
+                frame.setVisible(true);
+            }
+        });
+    }
+}
--- src/main/java/com/discordbot/command/MemeCommand.java
+++ src/main/java/com/discordbot/command/MemeCommand.java
@@ -1,109 +1,176 @@
 package com.discordbot.command;
 
-import java.awt.Font;
-import java.awt.FontMetrics;
-import java.awt.Graphics;
-import java.awt.geom.AffineTransform;
-import java.awt.image.BufferedImage;
-import java.io.ByteArrayInputStream;
-import java.io.File;
-import java.io.IOException;
-import java.io.InputStream;
-import java.awt.Graphics2D;
-
-import javax.imageio.ImageIO;
-
-import org.apache.commons.io.output.ByteArrayOutputStream;
-
+import com.discordbot.util.IOUtils;
 import net.dv8tion.jda.core.MessageBuilder;
 import net.dv8tion.jda.core.entities.ChannelType;
 import net.dv8tion.jda.core.entities.MessageChannel;
+import net.dv8tion.jda.core.utils.SimpleLog;
+import org.apache.commons.io.output.ByteArrayOutputStream;
+
+import javax.imageio.ImageIO;
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.Graphics2D;
+import java.awt.Shape;
+import java.awt.font.FontRenderContext;
+import java.awt.font.TextLayout;
+import java.awt.geom.AffineTransform;
+import java.awt.geom.Rectangle2D;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Path;
 
 @Command(tag = "meme")
 public class MemeCommand extends CommandListener {
-	
-	private static final String[] extensions = {"png", "jpg", "jpeg", "gif"};
 
-	public MemeCommand(CommandHandler handler) {
-		super(handler);
-	}
+    private static final SimpleLog LOG = SimpleLog.getLog("MemeCommand");
 
-	@Override
-	public void onCommandReceived(CommandReceivedEvent event) {
-		MessageChannel channel = event.getMessageReceivedEvent().getChannel();
-		java.util.List<String> args = event.getArgs();
-		
-		if (args.size() >= 2) {
-			String fileName = args.get(0);
-			
-			for (String ext : extensions) {
-				try {
-					File path = new File(System.getProperty("user.home") + "/meme/" + fileName + "." + ext);
-					final BufferedImage image = ImageIO.read(path);
+    private static final String[] extensions = {"png", "jpg", "jpeg", "gif"};
 
-					Graphics2D g = (Graphics2D) image.getGraphics();
-//					g.fillRect(0, 0, image.getWidth(), image.getHeight());
-					
-					FontMetrics fm = g.getFontMetrics();
-					System.out.println(fm.getAscent());
-					double scale = image.getHeight() / (fm.getHeight());
-					String draw = String.join(" ", args.subList(1, args.size()));
-					
-					Font font = new Font("Impact", Font.PLAIN, 20); // g.getFont().deriveFont(Font.PLAIN, AffineTransform.getScaleInstance(scale, scale));
-					g.setFont(font);
-					fm = g.getFontMetrics(font);
-					int xPos = (image.getWidth() - fm.stringWidth(draw)) / 2;
-					int yPos = ((image.getWidth() - fm.getHeight()) / 2) + fm.getAscent();
-					g.drawString(draw, xPos, yPos);
-					g.dispose();
-					
-					ByteArrayOutputStream baos = new ByteArrayOutputStream();
-					ImageIO.write(image, "png", baos);
-					InputStream is = new ByteArrayInputStream(baos.toByteArray());
-					
-					channel.sendFile(is, "Batman.png", (new MessageBuilder()).append("A message").build()).queue();
-					break;
-				} catch (IOException ignore) {
-					continue;
-				}
-				// TODO: meme doesn't exist
-			}
-		} else if (!args.isEmpty() && (args.get(0).equalsIgnoreCase("help") || args.get(0).equalsIgnoreCase("list"))) {
-			File folder = new File(System.getProperty("user.home") + "/meme");
-			File[] fileList = folder.listFiles();
-			
-			MessageBuilder messageBuilder = new MessageBuilder();
-			messageBuilder.append("Memes:");
-			
-			for (File file : fileList) {
-				String fileName = file.getName();
-				if (fileName.contains(".") && !fileName.endsWith(".")) {
-					String extension = fileName.substring(fileName.lastIndexOf('.') + 1);
-					for (String ext : extensions) {
-						if (ext.equals(extension)) {
-							messageBuilder.append("\n" + fileName.substring(0, fileName.lastIndexOf('.')));
-						}
-					}
-				}
-			}
-			
-			channel.sendMessage(messageBuilder.build()).queue();
-		}
-	}
+    public MemeCommand(CommandHandler handler) {
+        super(handler);
+    }
 
-	@Override
-	public boolean usesChannel(ChannelType type) {
-		return true;
-	}
+    @Override
+    public void onCommandReceived(CommandReceivedEvent event) {
+        MessageChannel channel = event.getMessageReceivedEvent().getChannel();
+        java.util.List<String> args = event.getArgs();
 
-	@Override
-	public String getDescription() {
-		return "Posts a meme with the caption you give it.";
-	}
+        if (args.isEmpty() || args.get(0).equalsIgnoreCase("help") || args.get(0).equalsIgnoreCase("list")) {
+            // they want to know what memes are available
+            Path path = IOUtils.getResourcePath("meme");
 
-	@Override
-	public String getHelp() {
-		return "Type the name of the meme followed by the caption.";
-	}
+            // make sure path exists
+            if (!path.toFile().exists()) {
+                if (path.toFile().mkdirs()) {
+                    LOG.warn("Resource folder did not exist, created at: " + path.toString());
+                    return;
+                } else {
+                    LOG.warn("Resource folder did not exist and could not be created. Check " + path.toString());
+                    return;
+                }
+            }
+
+            // get a list of files
+            File[] fileList = path.toFile().listFiles();
+
+            // make sure the file list isn't empty
+            if (fileList == null || fileList.length == 0) {
+                channel.sendMessage("No memes available, your admins need to step up their game!").queue();
+                return;
+            }
+
+            // send a message with the list of files
+            MessageBuilder messageBuilder = new MessageBuilder();
+            messageBuilder.append("Memes:");
+            for (File file : fileList) {
+                String fileName = file.getName();
+                if (fileName.contains(".") && !fileName.endsWith(".")) {
+                    String extension = fileName.substring(fileName.lastIndexOf('.') + 1);
+                    for (String ext : extensions) {
+                        if (ext.equals(extension)) {
+                            messageBuilder.append("\n").append(fileName.substring(0, fileName.lastIndexOf('.')));
+                        }
+                    }
+                }
+            }
+            channel.sendMessage(messageBuilder.build()).queue();
+        } else if (args.size() >= 2) {
+            // they want a meme, so let's give them one
+            String fileName = args.get(0);
+
+            for (String ext : extensions) {
+                try {
+                    // load the meme
+                    File file = IOUtils.getResourcePath("meme", fileName + '.' + ext).toFile();
+                    if (!file.exists()) {
+                        continue;
+                    }
+                    final BufferedImage image = ImageIO.read(file);
+
+                    // get the meme graphics and bounds
+                    Graphics2D graphics = (Graphics2D) image.getGraphics();
+                    Rectangle2D topBounds = new Rectangle2D.Double(5, 5,
+                            image.getWidth() - 10, image.getHeight() / 4);
+                    Rectangle2D bottomBounds = new Rectangle2D.Double(5, image.getHeight() - 5,
+                            image.getWidth() - 10, image.getHeight() / 4);
+
+                    // add text to the meme
+                    graphics.setFont(new Font("Impact", Font.PLAIN, 30));
+                    addTextToGraphics(graphics, topBounds, args.get(1), Position.TOP);
+                    if (args.size() >= 3) {
+                        addTextToGraphics(graphics, bottomBounds, args.get(2), Position.BOTTOM);
+                    }
+                    graphics.dispose();
+
+                    // convert the meme to an InputStream
+                    ByteArrayOutputStream baos = new ByteArrayOutputStream();
+                    ImageIO.write(image, "png", baos);
+                    ImageIO.write(image, "png", new File("test.png"));
+
+                    // send the meme
+                    channel.sendFile(baos.toByteArray(), args.get(0) + ".png", (new MessageBuilder()).append("...").build()).queue();
+                    return;
+                } catch (IOException e) {
+                    LOG.log(e);
+                }
+            }
+            channel.sendMessage("There are no memes by the name " + args.get(0)).queue();
+        } else {
+            // no text supplied
+            channel.sendMessage("Blank memes aren't fun").queue();
+        }
+    }
+
+    public void addTextToGraphics(Graphics2D graphics2D, Rectangle2D bounds, String text, Position position) {
+        // create a new graphics so that we can add text without changing graphic2D's settings
+        Graphics2D graphics = (Graphics2D)graphics2D.create();
+
+        // calculate the scale of the text
+        Font font = graphics.getFont();
+        FontRenderContext frc = graphics.getFontRenderContext();
+        TextLayout textLayout = new TextLayout(text, font, frc);
+
+        double scale = Math.min(bounds.getHeight() / (textLayout.getBounds().getHeight()),
+                bounds.getWidth() / (textLayout.getBounds().getWidth()));
+
+        // convert the text to a shape
+        Shape outline = textLayout.getOutline(null);
+        Rectangle2D outlineBounds = outline.getBounds2D();
+
+        // set the position to draw the text
+        double xPos = bounds.getX() / scale + bounds.getWidth() / scale / 2 - outlineBounds.getWidth() / 2
+                - outlineBounds.getX();
+        double yPos = bounds.getY() / scale + outlineBounds.getHeight() * (position == Position.TOP ? 0 : -1)
+                - outlineBounds.getY();
+        AffineTransform transform = AffineTransform.getScaleInstance(scale, scale);
+        transform.translate(xPos, yPos);
+        graphics.transform(transform);
+
+        // draw the text
+        graphics.setColor(Color.WHITE);
+        graphics.fill(outline);
+        graphics.setColor(Color.BLACK);
+        graphics.draw(outline);
+        graphics.dispose();
+    }
+
+    @Override
+    public boolean usesChannel(ChannelType type) {
+        return true;
+    }
+
+    @Override
+    public String getDescription() {
+        return "Posts a meme with the caption you give it.";
+    }
+
+    @Override
+    public String getHelp() {
+        return "Type the name of the meme followed by the caption.";
+    }
+
+    private enum Position { TOP, BOTTOM }
 
 }
